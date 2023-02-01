5f119ccf6c2778d3b038a8338f588571d57bce00
Mashfuk Hossain <mashfuk@gmail.com>
2023-02-01 13:16:31 +0600

    create register page

--- pages/register.js
+++ pages/register.js
@@ -15,41 +15,58 @@ import {
   import useStyles from '../utils/styles';
   import Cookies from 'js-cookie';
   
-  export default function Login() {
+  export default function Register() {
     const router = useRouter();
-    const { redirect } = router.query; // login?redirect=/shipping
+    const { redirect } = router.query;
     const { state, dispatch } = useContext(Store);
     const { userInfo } = state;
     useEffect(() => {
       if (userInfo) {
-      router.push('/');
+        router.push('/');
       }
     }, []);
-
+  
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const classes = useStyles();
     const submitHandler = async (e) => {
       e.preventDefault();
+      if (password !== confirmPassword) {
+        alert("passwords don't match");
+        return;
+      }
       try {
-        const { data } = await axios.post('/api/users/login', {
+        const { data } = await axios.post('/api/users/register', {
+          name,
           email,
           password,
         });
         dispatch({ type: 'USER_LOGIN', payload: data });
-        userInfo: Cookies.get('userInfo') ? JSON.parse.userInfo : null
+        Cookies.set('userInfo', data);
         router.push(redirect || '/');
       } catch (err) {
         alert(err.response.data ? err.response.data.message : err.message);
       }
     };
     return (
-      <Layout title="Login">
+      <Layout title="Register">
         <form onSubmit={submitHandler} className={classes.form}>
           <Typography component="h1" variant="h1">
-            Login
+            Register
           </Typography>
           <List>
+            <ListItem>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="name"
+                label="Name"
+                inputProps={{ type: 'text' }}
+                onChange={(e) => setName(e.target.value)}
+              ></TextField>
+            </ListItem>
             <ListItem>
               <TextField
                 variant="outlined"
@@ -71,14 +88,24 @@ import {
               ></TextField>
             </ListItem>
             <ListItem>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="confirmPassword"
+                label="Confirm Password"
+                inputProps={{ type: 'password' }}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              ></TextField>
+            </ListItem>
+            <ListItem>
               <Button variant="contained" type="submit" fullWidth color="primary">
-                Login
+                Register
               </Button>
             </ListItem>
             <ListItem>
-              Do not have an account? &nbsp;
-              <NextLink href={`/register?redirect=${redirect || '/'}`} passHref>
-                <Link>Register</Link>
+              Already have an account? &nbsp;
+              <NextLink href={`/login?redirect=${redirect || '/'}`} passHref>
+                <Link>Login</Link>
               </NextLink>
             </ListItem>
           </List>
