ea97c10889db1160f6e6a8000fc4803ba6bb3a9c
Mashfuk Hossain <mashfuk@gmail.com>
2023-02-13 13:30:49 +0600

    user profile page

--- pages/login.js
+++ pages/login.js
@@ -16,6 +16,7 @@ import {
   import Cookies from 'js-cookie';
   import { Controller, useForm } from 'react-hook-form';
   import { useSnackbar } from 'notistack';
+  import { getError } from '../utils/error';
   
   export default function Login() {
     const {
@@ -47,10 +48,7 @@ import {
         userInfo: Cookies.get('userInfo') ? JSON.parse.userInfo : null
         router.push(redirect || '/');
       } catch (err) {
-        enqueueSnackbar(
-          err.response.data ? err.response.data.message : err.message,
-          { variant: 'error' }
-        );
+        enqueueSnackbar(getError(err), { variant: 'error' });
       }
     };
     return (
